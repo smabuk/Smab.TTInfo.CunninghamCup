8dd25c1680d04abb43c3ee146146867f2cf2ccc4
smabuk <2011834+smabuk@users.noreply.github.com>
2025-08-16 17:05:41 +0100

    Add project files.

---
+++ Smab.TTInfo.CunninghamCup/Smab.TTInfo.CunninghamCup/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Smab.TTInfo.CunninghamCup
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Smab.TTInfo.CunninghamCup" };
+        }
+    }
+}
---
+++ Smab.TTInfo.CunninghamCup/Smab.TTInfo.CunninghamCup/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Smab.TTInfo.CunninghamCup
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
