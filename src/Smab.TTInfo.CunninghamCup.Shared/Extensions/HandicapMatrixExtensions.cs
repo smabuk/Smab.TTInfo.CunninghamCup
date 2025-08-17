55bef2dbcda5a895cd8dc497452dc0eddc3f70b3
smabuk <2011834+smabuk@users.noreply.github.com>
2025-08-17 13:53:15 +0100

    Refactor HandicapMatrix and add extension methods
    Changed `HandicapMatrix` from a record to a class and added a static dictionary `DefaultMatrix` for default handicap values. Updated project files to include new `<LangVersion>` properties. Introduced `HandicapMatrixExtensions` with methods to retrieve handicap values based on differences and handicaps.

---
+++ src/Smab.TTInfo.CunninghamCup.Shared/Extensions/HandicapMatrixExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Smab.TTInfo.CunninghamCup.Shared.Models;
+
+namespace Smab.TTInfo.CunninghamCup.Shared.Extensions;
+public static class HandicapMatrixExtensions
+{
+	extension(HandicapMatrix)
+	{
+		public static int GetStart(int difference)
+			=> HandicapMatrix.DefaultMatrix.TryGetValue(int.Abs(difference), out var start)
+				? start
+				: 18;
+
+		public static int GetStart(int handicapA, int handicapB)
+			=> HandicapMatrix.DefaultMatrix.TryGetValue(int.Abs(handicapA - handicapB), out var start)
+				? start
+				: 18;
+	}
+}
--- src/Smab.TTInfo.CunninghamCup.Shared/Models/HandicapMatrix.cs
+++ src/Smab.TTInfo.CunninghamCup.Shared/Models/HandicapMatrix.cs
@@ -1,5 +1,37 @@
 ﻿namespace Smab.TTInfo.CunninghamCup.Shared.Models;
 
-public record HandicapMatrix(
-	IReadOnlyList<HandicapMatrixEntry> PlayerHandicaps
-);
+public class HandicapMatrix
+{
+	public readonly static IReadOnlyDictionary<int, int> DefaultMatrix =
+		new Dictionary<int, int>
+		{
+			{ 00, 00 },
+			{ 01, 01 },
+			{ 02, 02 },
+			{ 03, 03 },
+			{ 04, 04 },
+			{ 05, 05 },
+			{ 06, 06 },
+			{ 07, 07 },
+			{ 08, 08 },
+			{ 09, 09 },
+			{ 10, 10 },
+			{ 11, 11 },
+			{ 12, 12 },
+			{ 13, 12 },
+			{ 14, 13 },
+			{ 15, 13 },
+			{ 16, 14 },
+			{ 17, 14 },
+			{ 18, 15 },
+			{ 19, 15 },
+			{ 20, 16 },
+			{ 21, 16 },
+			{ 22, 17 },
+			{ 23, 17 },
+			{ 24, 17 },
+			{ 25, 18 },
+			{ 26, 18 },
+			{ 27, 18 }
+		};
+}
